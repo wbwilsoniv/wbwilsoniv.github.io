619bc0e6701f9a4cb74866024d4af14c4558184c
wbwilsoniv <wbwilsoniv@gmail.com>
2019-01-29 21:04:59 -0500

    added mobile event listeners to show and hide details

--- scripts.js
+++ scripts.js
@@ -14,30 +14,44 @@ function showHamburger() {
 }
 
 function pageLoad() {
+  let handleTouchShow = function(e) {
+    e.preventDefault();
+    show(e.target.id);
+    console.log(e.target.id);
+  };
+
   let details = document.getElementsByClassName("deets");
   let deetArr = Array.from(details);
   deetArr.map(item => {
-    item.addEventListener("click touchstart", e => {
+    item.addEventListener("touchstart", handleTouchShow, false);
+    item.addEventListener("click", e => {
       e.preventDefault();
       show(item.id);
     });
   });
 
+  let handleTouchHide = function(e) {
+    e.preventDefault();
+    hideDetails(e.target.id);
+    console.log(hamburg);
+  };
+
   let hide = document.getElementsByClassName("hide");
   let hideArr = Array.from(hide);
   hideArr.map(item => {
-    item.addEventListener("click touchstart", e => {
+    item.addEventListener("touchstart", handleTouchHide, false);
+    item.addEventListener("click", e => {
       e.preventDefault();
       hideDetails(item.id);
     });
   });
-  let handleTouch = function(e) {
+  let handleTouchHam = function(e) {
     e.preventDefault();
     console.log(hamburg);
     showHamburger();
   };
 
   let hamburg = document.getElementById("ham");
-  hamburg.addEventListener("touchstart", handleTouch, false);
+  hamburg.addEventListener("touchstart", handleTouchHam, false);
 }
 pageLoad();
